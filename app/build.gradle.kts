dfec09818f8f5dc193bd8b6aef5309b6ef486584
takusan23 <takumu1200@gmail.com>
2025-08-31 15:12:31 +0900

    Compose / Gradle / Kotlin / ライブラリ の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
 
     // JetpackCompose BOM指定
-    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
